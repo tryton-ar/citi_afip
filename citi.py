b22e73155baf9eced4a7de7f6418cea5b9216f09
Luciano Rossi <lukio@gcoop.coop>
2018-05-28 09:55:11 -0300

    fixes #16 add order by invoice_date

--- citi.py
+++ citi.py
@@ -175,7 +175,7 @@ class CitiWizard(Wizard):
             ('type', '=', 'out'), # Invoice, Credit Note
             ('move.period', '=', self.start.period),
             ('pos.pos_do_not_report', '=', False),
-        ])
+        ], order=[('invoice_date', 'ASC')])
         lines = ""
         for invoice in invoices:
             tipo_comprobante = invoice.invoice_type.invoice_type.rjust(3,'0')
@@ -228,7 +228,7 @@ class CitiWizard(Wizard):
             ('type', '=', 'out'), # Invoice, Credit Note
             ('move.period', '=', self.start.period),
             ('pos.pos_do_not_report', '=', False),
-        ])
+        ], order=[('invoice_date', 'ASC')])
         lines = ""
         for invoice in invoices:
             alicuotas = {
@@ -389,7 +389,7 @@ class CitiWizard(Wizard):
             ('state', 'in', ['posted', 'paid']),
             ('type', '=', 'in'), # Supplier Invoice, Supplier Credit Note
             ('move.period', '=', self.start.period),
-        ])
+        ], order=[('invoice_date', 'ASC')])
         lines = ""
         for invoice in invoices:
             tipo_comprobante = invoice.tipo_comprobante
@@ -441,7 +441,7 @@ class CitiWizard(Wizard):
             ('state', 'in', ['posted', 'paid']),
             ('type', '=', 'in'), # Supplier Invoice, Supplier Credit Note
             ('move.period', '=', self.start.period),
-        ])
+        ], order=[('invoice_date', 'ASC')])
 
         lines = ""
         for invoice in invoices:
