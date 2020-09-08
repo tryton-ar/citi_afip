67f354f347b4b537c459890f67813f94688550b1
Adrián Bernardi <adrian.bernardi@gmail.com>
2020-09-08 20:19:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ import re
 from configparser import ConfigParser
 from setuptools import setup
 
+MODULE = 'citi_afip'
+PREFIX = 'trytonar'
 MODULE2PREFIX = {
     'account_invoice_ar': 'trytonar',
     'party_ar': 'trytonar',
@@ -34,7 +36,7 @@ def get_require_version(name):
 
 
 config = ConfigParser()
-config.read_file(open('tryton.cfg'))
+config.read_file(open(os.path.join(os.path.dirname(__file__), 'tryton.cfg')))
 info = dict(config.items('tryton'))
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
@@ -43,7 +45,6 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_citi_afip'
 
 download_url = 'https://github.com/tryton-ar/citi_afip/tree/%s.%s' % (
     major_version, minor_version)
@@ -68,20 +69,26 @@ requires.append(get_require_version('trytond'))
 tests_require = [get_require_version('proteus')]
 dependency_links = list(LINKS.values())
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='Tryton module to export Compras y Ventas RG3685 (ex CITI)',
     long_description=read('README.rst'),
     author='tryton-ar',
     url='https://github.com/tryton-ar/citi_afip',
     download_url=download_url,
-    package_dir={'trytond.modules.citi_afip': '.'},
+    project_urls={
+        "Bug Tracker": 'https://bugs.tryton.org/',
+        "Documentation": 'https://docs.tryton.org/',
+        "Forum": 'https://www.tryton.org/forum',
+        "Source Code": 'https://github.com/tryton-ar/citi_afip',
+        },
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.citi_afip',
-        'trytond.modules.citi_afip.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.citi_afip': (info.get('xml', []) + [
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
             'tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst',
             'tests/*.txt']),
         },
@@ -92,8 +99,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later'
-        ' (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
@@ -114,8 +121,8 @@ setup(name=name,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    citi_afip = trytond.modules.citi_afip
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
