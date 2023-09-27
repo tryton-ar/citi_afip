335dcbe89f0c2efd844b675b5ef467f2becc9856
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-26 22:43:31 -0300

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-
-try:
-    from trytond.modules.citi_afip.tests.test_citi_afip import suite
-except ImportError:
-    from .test_citi_afip import suite
-
-__all__ = ['suite']
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
