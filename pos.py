1a7b3bd68ea40d611514dea0e564a2d9415f5cb0
Luciano Rossi <lukio@gcoop.coop>
2025-04-20 11:54:00 -0300

    Prepare release 7.0

--- pos.py
+++ pos.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # This file is part of the citi_afip module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
