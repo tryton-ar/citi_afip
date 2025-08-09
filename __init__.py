77a3d0ec6cda7e6b613e4924bd9ce2451f26922f
Adrián Bernardi <adrian@silix.com.ar>
2025-08-08 21:43:04 -0300

    Deprecate module

--- __init__.py
+++ __init__.py
@@ -1,20 +1,3 @@
 # This file is part of the citi_afip module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-
-from . import citi
-from . import pos
-
-__all__ = ['register']
-
-def register():
-    Pool.register(
-        pos.Pos,
-        citi.CitiStart,
-        citi.CitiExportar,
-        module='citi_afip', type_='model')
-    Pool.register(
-        citi.CitiWizard,
-        module='citi_afip', type_='wizard')
